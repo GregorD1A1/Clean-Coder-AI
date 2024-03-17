681d7d9b280ff84aaba3362775c9c599375f0381
GregorD1A1 <dudnikgrv@gmail.com>
2024-03-17 11:58:13 +0100

    small changes

--- langgraph_coder_pipeline.py
+++ langgraph_coder_pipeline.py
@@ -5,16 +5,16 @@ from langgraph_coder.langgraph_executor import Executor, check_file_contents
 
 task = ("check if creation step 2 (located in components)is optimal written.")
 
-'''
+
 files = research_task(task)
 file_contents = check_file_contents(files)
 
 planer = Planer(task, file_contents)
 plan = planer.plan()
-'''
-file_contents = "dzikie dinożarły"
-plan = "finish work and go home."
-files = ["src/components/PostCreationStep2.vue"]
+
+#file_contents = "dzikie dinożarły"
+#plan = "finish work and go home."
+#files = ["src/components/PostCreationStep2.vue"]
 
 executor = Executor(files)
 executor.do_task(task, plan, file_contents)
