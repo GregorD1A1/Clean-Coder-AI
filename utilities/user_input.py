713f1f93e87a2ae2e889e36c6761b26f39a6dc3a
Grigorij Dudnik <dudnikgrv@gmail.com>
2024-10-04 14:40:06 +0200

    docker support added:

--- utilities/user_input.py
+++ utilities/user_input.py
@@ -1,47 +1,9 @@
-from pynput.keyboard import Key, Listener
 from utilities.voice_utils import VoiceRecorder
 from utilities.util_functions import print_formatted
+import keyboard
+
 
 recorder = VoiceRecorder()
-
-# Old try, to remove
-class InputHandler():
-    def __init__(self):
-        self.output_string = ""
-        self.been_recorded = False
-
-    def user_input(self, prompt=""):
-        print_formatted("Just start writing or record voice message by pressing Tab:", color="yellow", bold=True)
-
-        def press_interrupt(key):
-            if hasattr(key, 'char'):
-                if not recorder.is_recording:
-                    return False
-                else:
-                    if key.char == 'c':
-                        recorder.stop_recording()
-                        print("Recording canceled")
-
-            elif key == Key.tab:
-                recorder.start_recording()
-                self.been_recorded = True
-
-            elif key == Key.enter:
-                if recorder.is_recording:
-                    recorder.stop_recording()
-                    print("Recording finished")
-                    self.output_string = recorder.transcribe_audio()
-                return False  # Stop listener
-
-        # Collect all event until released
-        with Listener(on_press=press_interrupt) as listener:
-            listener.join()
-
-        if not self.been_recorded:
-            self.output_string = input()
-
-        return self.output_string
-
 
 
 def user_input(prompt=""):
@@ -61,17 +23,7 @@ def user_input(prompt=""):
 
 def record_voice_message():
     recorder.start_recording()
-
-    def press_interrupt(key):
-        if key == Key.enter:
-            recorder.stop_recording()
-            print("Recording finished.\n")
-            return False  # Stop listener
-
-    with Listener(on_press=press_interrupt, suppress=True) as listener:
-        listener.join()
-
+    keyboard.wait('enter')
+    recorder.stop_recording()
+    print("Recording finished.\n")
     return recorder.transcribe_audio()
-
-if __name__ == "__main__":
-    print(user_input("Provide your feedback."))
