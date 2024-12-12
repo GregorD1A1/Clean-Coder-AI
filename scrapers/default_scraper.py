d8ce85c6b8f736bf53dbf5f206893dc65ae49c59
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2024-12-12 13:36:33 +0000

    match local with remote master. Only testing added

--- scrapers/default_scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def execute_scrape(url, work_dir):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response
-    else:
-        return response.status_code
--- single_task_coder.py
+++ single_task_coder.py
@@ -1,17 +1,25 @@
 if __name__ == "__main__":
     from utilities.graphics import print_ascii_logo
     print_ascii_logo()
+from dotenv import find_dotenv
+from utilities.set_up_dotenv import set_up_env_coder_pipeline
+if not find_dotenv():
+    set_up_env_coder_pipeline()
 
 from agents.researcher_agent import Researcher
 from agents.planner_agent import planning
 from agents.executor_agent import Executor
 from agents.debugger_agent import Debugger
+from agents.frontend_feedback import write_screenshot_codes, execute_screenshot_codes
 import os
 from utilities.user_input import user_input
 from utilities.print_formatters import print_formatted
+from utilities.start_project_functions import set_up_dot_clean_coder_dir
+from utilities.util_functions import create_frontend_feedback_story
+from concurrent.futures import ThreadPoolExecutor
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+use_frontend_feedback = bool(os.getenv("FRONTEND_PORT"))
 
 
 def run_clean_coder_pipeline(task, work_dir):
@@ -21,17 +29,31 @@ def run_clean_coder_pipeline(task, work_dir):
     plan = planning(task, file_paths, image_paths, work_dir)
 
     executor = Executor(file_paths, work_dir)
-    test_instruction, file_paths = executor.do_task(task, plan)
-    # print_formatted(test_instruction, color="blue")
+
+    if use_frontend_feedback:
+        create_frontend_feedback_story()
+        with ThreadPoolExecutor() as executor_thread:
+            future = executor_thread.submit(write_screenshot_codes, task, plan, work_dir)
+            test_instruction, file_paths = executor.do_task(task, plan)
+            playwright_codes, screenshot_descriptions = future.result()
+        if playwright_codes:
+            print_formatted("Making screenshots, please wait a while...", color="light_blue")
+            first_vfeedback_screenshots_msg = execute_screenshot_codes(playwright_codes, screenshot_descriptions)
+        else:
+            first_vfeedback_screenshots_msg = None
+    else:
+        test_instruction, file_paths = executor.do_task(task, plan)
+        first_vfeedback_screenshots_msg = None
 
     human_message = user_input("Please test app and provide commentary if debugging/additional refinement is needed.")
     if human_message in ['o', 'ok']:
         return
-    debugger = Debugger(file_paths, work_dir, human_message)
-    debugger.do_task(task, plan, file_paths)
+    debugger = Debugger(file_paths, work_dir, human_message, first_vfeedback_screenshots_msg, playwright_codes, screenshot_descriptions)
+    debugger.do_task(task, plan)
 
 
 if __name__ == "__main__":
-    task = user_input("Provide task to be executed.")
     work_dir = os.getenv("WORK_DIR")
+    set_up_dot_clean_coder_dir(work_dir)
+    task = user_input("Provide task to be executed.")
     run_clean_coder_pipeline(task, work_dir)
